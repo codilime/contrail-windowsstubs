d47c7dafa0881147b4facaae2a65edb8c11f2703
sagarkchitnis <sagarc@juniper.net>
2017-06-12 06:55:05 -0700

    Create TaskUtil.cpp

--- WindowsStubs/WindowsStubs/TaskUtil.cpp
+++ WindowsStubs/WindowsStubs/TaskUtil.cpp
@@ -8,85 +8,88 @@
 
 bool TaskExecuteAndWait(std::string execpath, std::string *pOutput)
 {
-    STARTUPINFO si;
-    PROCESS_INFORMATION pi;
-    SECURITY_ATTRIBUTES securityAttr;
-    HANDLE inputPipe, outputPipe;
-    bool retval = true;
-    ZeroMemory(&si, sizeof(si));
-    si.cb = sizeof(si);
-    ZeroMemory(&pi, sizeof(pi));
-    ZeroMemory(&securityAttr, sizeof(securityAttr));
-    securityAttr.nLength = sizeof(securityAttr);
-    securityAttr.bInheritHandle = TRUE;
-    int len;
-    int slength = (int)execpath.length() + 1;
-    len = MultiByteToWideChar(CP_ACP, 0, execpath.c_str(), slength, 0, 0);
-    wchar_t* buf = new wchar_t[len];
-    MultiByteToWideChar(CP_ACP, 0, execpath.c_str(), slength, buf, len);
-    if (pOutput)
-    {
-        ::CreatePipe(&inputPipe, &outputPipe, &securityAttr, 0);
-        si.dwFlags = STARTF_USESTDHANDLES;
-        si.hStdInput = NULL;
-        si.hStdOutput = outputPipe;
-        si.hStdError = outputPipe;
-    }
+	
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	SECURITY_ATTRIBUTES securityAttr;
+	HANDLE inputPipe, outputPipe;
+	std::string output;
 
-    if (!CreateProcess(NULL,   // No module name (use command line)
-        buf,        // Command line
-        NULL,           // Process handle not inheritable
-        NULL,           // Thread handle not inheritable
-        TRUE,          // Set handle inheritance to FALSE
-        0,              // No creation flags
-        NULL,           // Use parent's environment block
-        NULL,           // Use parent's starting directory 
-        &si,            // Pointer to STARTUPINFO structure
-        &pi)           // Pointer to PROCESS_INFORMATION structure
-        )
-    {
-        DWORD le = GetLastError();
-        std::cout << "CreateProcess Failed. Error code:" << le << std::endl;
-        retval = false;
-        goto cleanup;
-    }
-    
-    if (WaitForSingleObject(pi.hProcess, INFINITE) == WAIT_FAILED)
-    {
-        DWORD le = GetLastError();
-        std::cout << "WaitForSingleObject Failed. Error code:" << le << std::endl;
-        retval = false;
-        goto cleanup;
-    }
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+	ZeroMemory(&securityAttr, sizeof(securityAttr));
+	securityAttr.nLength = sizeof(securityAttr);
+	securityAttr.bInheritHandle = TRUE;
+	int len;
+	int slength = (int)execpath.length() + 1;
+	len = MultiByteToWideChar(CP_ACP, 0, execpath.c_str(), slength, nullptr, 0);
+	wchar_t* buf = new wchar_t[len];
+	MultiByteToWideChar(CP_ACP, 0, execpath.c_str(), slength, buf, len);
+	if (usePipes) {
 
-    if (pOutput)
-    {
-        CloseHandle(outputPipe);
-        const int BUFFER_SIZE = 200;
-        DWORD nNumberOfBytesToRead = 0;
-        do
-        {
-            char buf[BUFFER_SIZE + 1];
-        
-            if (::ReadFile(inputPipe, buf, BUFFER_SIZE, &nNumberOfBytesToRead, 0))
-            {
-                if (nNumberOfBytesToRead == 0)
-                    break;
 
-                buf[nNumberOfBytesToRead] = '\0';
+		::CreatePipe(&inputPipe, &outputPipe, &securityAttr, 0);
 
-                *pOutput += buf;
-            }
+		si.dwFlags = STARTF_USESTDHANDLES;
+		si.hStdInput = NULL;
+		si.hStdOutput = outputPipe;
+		si.hStdError = outputPipe;
+	}
 
-        } while (nNumberOfBytesToRead != 0);
-        pOutput->erase(std::remove_if(pOutput->begin(), pOutput->end(), [](char c) { return std::isspace(c); }), pOutput->end());
-        CloseHandle(inputPipe);
-    }
+	if (!CreateProcess(NULL,   // No module name (use command line)
+		buf,        // Command line
+		NULL,           // Process handle not inheritable
+		NULL,           // Thread handle not inheritable
+		TRUE,          // Set handle inheritance to FALSE
+		0,              // No creation flags
+		NULL,           // Use parent's environment block
+		NULL,           // Use parent's starting directory 
+		&si,            // Pointer to STARTUPINFO structure
+		&pi)           // Pointer to PROCESS_INFORMATION structure
+		)
+	{
+		delete[] buf;
+		DWORD le = GetLastError();
+		std::cout << "CreateProcess Failed. Error code:" << le << std::endl;
+		
+		return "-1";
+	}
 
-cleanup: //goto is very commonly used in Microsoft sample code.to make code look cleaner and avoid too many ifs.
-    CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
-    delete[] buf;
-    return true;
+	
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	if (usePipes)
+	{
+
+		CloseHandle(outputPipe);
+
+		const int BUFFER_SIZE = 200;
+		DWORD NumberOfBytesRead = 0;
+		do
+		{
+			char buf[BUFFER_SIZE+1] = "";
+		
+			if (::ReadFile(inputPipe, buf, BUFFER_SIZE, &NumberOfBytesRead, 0))
+			{
+				if (NumberOfBytesRead == 0)
+					break;
+
+				buf[NumberOfBytesRead] = '\0';
+
+				output += std::string(buf);
+			}
+
+		} while (NumberOfBytesRead != 0);
+
+		CloseHandle(inputPipe);
+
+	}
+
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+	delete[] buf;
+	output.erase(std::remove_if(output.begin(), output.end(), [](char c) { return std::isspace(c); }), output.end());
+	return output;
 }
 
