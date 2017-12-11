d98772dd0bdf8abe30ca481d7001205d8ead1569
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2017-12-11 14:48:29 +0100

    Move code to controller/src/base

--- inc/unistd.h
+++ inc/unistd.h
@@ -2,11 +2,13 @@
 
 #include <io.h>
 #include <process.h>
+#include <BaseTsd.h>
 
 #define STDOUT_FILENO   1
 
 typedef unsigned int useconds_t;
 typedef unsigned int pid_t;
+typedef SSIZE_T ssize_t;
 
 #ifdef __cplusplus
 extern "C" {
