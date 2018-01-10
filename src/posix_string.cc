4e38c201c53ee40bb0f5f362c1e98896788f9c2a
Michal Clapinski <michal.clapinski@codilime.com>
2018-01-10 16:45:20 +0100

    review fixes

--- src/posix_string.cc
+++ src/posix_string.cc
@@ -1,5 +1,7 @@
 #include <posix_string.h>
 
+#include <cstdio>
+
 constexpr size_t CURRENT_SIGNAL_NAME_SIZE = 128;
 
 static char current_signal_name[CURRENT_SIGNAL_NAME_SIZE];
--- src/sys/wait.cc
+++ src/sys/wait.cc
@@ -1,4 +1,4 @@
-#include <wait.h>
+#include <sys/wait.h>
 
 pid_t waitpid(pid_t pid, int *status, int options) {
     return -1;
